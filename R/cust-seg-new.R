45de105cd1c3edf1e6f3fe755c0cd9d7cc3ce594
Brandon Loudermilk <b.c.loudermilk@gmail.com>
2015-12-20 17:47:16 -0600

    prettified and tested intuition

--- R/cust-seg-new.R
+++ R/cust-seg-new.R
@@ -21,7 +21,13 @@ DEF_SEG_SDS <- matrix( c(
   4, NA, 10000, NA, NA, NA ), ncol=length(variable_names), byrow=TRUE)
 
 
-#' Simulate dummy marketing analtics data
+#' @title Simulate dummy marketing analtics data
+#' 
+#' @description Return data.frame of simulated, segmented marketing data
+#' 
+#' @details Given the sample segmentation, out_df has columns for each
+#' of the vars in variable_names plus a single column for 'Segment'. out_df 
+#' has rows equal to the sum of the segment sample counts in segment_sizes. 
 #' 
 #' @param variable_names character vector of demographic variable names
 #'        (e.g., c("age","gender"))
@@ -49,34 +55,57 @@ generateData <- function(variable_names = DEF_VAR_NAMES,
   set.seed(02554)
   # iterate over segments and create data for each
   for (i in seq_along(segment_names)) {
+    
+    # write out segment names
     cat(i, segment_names[i], "\n")
     
     # empty matrix to hold this particular segment’s data
-    this.seg <- data.frame(matrix(NA, nrow=segment_sizes[i], ncol=length(variable_names)))
+    df <- data.frame(matrix(NA, 
+                            nrow=segment_sizes[i], 
+                            ncol=length(variable_names)))
     
     # within segment, iterate over variables and draw appropriate random data
     for (j in seq_along(variable_names)) { # and iterate over each variable
+      
       if (variable_distributions[j] == "norm") { # draw random normals
-        this.seg[,j] <- rnorm(segment_sizes[i], mean=segment_means[i,j], sd=segment_sds[i,j])
-      } else if (variable_distributions[j] == "pois") { # draw counts
-        this.seg[, j] <- rpois(segment_sizes[i], lambda=segment_means[i, j])
-      } else if (variable_distributions[j] == "binom") { # draw binomials
-        this.seg[, j] <- rbinom(segment_sizes[i], size=1, prob=segment_means[i, j])
-      } else {
-        stop("Bad segment data type: ", variable_distributions[j])
+        
+        df[, j] <- rnorm(segment_sizes[i], 
+                         mean=segment_means[i,j], 
+                         sd=segment_sds[i,j])
+      
+        } else if (variable_distributions[j] == "pois") { # draw counts
+        
+          df[, j] <- rpois(segment_sizes[i], 
+                         lambda=segment_means[i, j])
+      
+        } else if (variable_distributions[j] == "binom") { # draw binomials
+        
+          df[, j] <- rbinom(segment_sizes[i], 
+                          size=1, 
+                          prob=segment_means[i, j])
+      
+        } else {
+        
+          stop("Bad segment data type: ", variable_distributions[j])
       }
     }
-    seg_df <- rbind(seg_df, this.seg)
+    seg_df <- rbind(seg_df, df)
   }
   
   # make the data frame names match what we defined
   names(seg_df) <- variable_names
+  
+  # TODO: THIS IS HARD-WIRED BELOW e.g., seg_df$ownHome
+  # WHAT HAPPEMS WHEN DEFAULT VARIABLES CHANGE?
+  
   # add segment membership for each row
   seg_df$Segment <- factor(rep(segment_names, times=segment_sizes))
+  
   # convert the binomial variables to nicely labeled factors
   seg_df$ownHome <- factor(seg_df$ownHome, labels=c("ownNo", "ownYes"))
   seg_df$gender <- factor(seg_df$gender, labels=c("Female", "Male"))
   seg_df$subscribe <- factor(seg_df$subscribe, labels=c("subNo", "subYes"))
+  
   return(seg_df)
   
 }
