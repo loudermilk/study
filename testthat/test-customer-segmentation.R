53b732888a9eb90fb42f6f33a77398e2384d2fb8
Brandon Loudermilk <b.c.loudermilk@gmail.com>
2015-12-20 14:53:31 -0600

    initial commit for marketing analytics

---
+++ testthat/test-customer-segmentation.R
@@ -0,0 +1,24 @@
+# test-customer-segmentation.R
+
+library(assertthat)
+library(testthat)
+
+#' Test that refactored code generates the same data structure as the original code
+testthat::test_that("replicated_df", {
+  orig_df <- generateDataOrig()
+  new_df <- generateData()
+  
+  assertthat::assert_that(class(orig_df) == class(new_df))
+  
+  # all cell comparisons should be TRUE
+  comparison_res <- (orig_df == new_df)
+  length <- length(comparison_res)
+  total <- sum(comparison_res)
+  assertthat::assert_that(length == total)
+  
+  orig_names <- names(orig_df)
+  new_names <- names(new_df)
+  assertthat::assert_that(length(orig_names) == sum(orig_names == new_names))
+  assertthat::assert_that(1 == 1)
+  
+})
